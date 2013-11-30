67ecd327bb70d121482f21a593c4bd387d7b258b
ktelfon <ktelfon@gmail.com>
2013-12-01 01:21:07 +0200

    Loads to textField but needs work on submit and more

--- src/java/tour/profile/Profile.java
+++ src/java/tour/profile/Profile.java
@@ -1,7 +1,12 @@
 package tour.profile;
 
+import java.util.Iterator;
+import java.util.List;
 import mail.account.Account;
+import mail.account.AccountDAO;
 import mail.menupanel.MenuPanel;
+import mail.session.SignInSession;
+import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
@@ -15,40 +20,100 @@ import org.apache.wicket.model.PropertyModel;
  */
 public final class Profile extends WebPage {
 
-    Account acc = new Account();
+    private Account acc = new Account();
+    private AccountDAO aO = new AccountDAO();
+    private String name;
+    private String surname;
+    private String age;
+    private String persCode;
 
     public Profile() {
         add(new MenuPanel("menuPanel"));
+
+        List<Account> accounts = aO.getAccountList();
+        SignInSession session = getMySession();
+        Account currentAccount = getAccount(accounts, session.getUser());
+
+        setName(currentAccount.getName());
+        setSurname(currentAccount.getSurname());
+        setAge(currentAccount.getAge());
+        setPersCode(currentAccount.getPersCode());
         
-        final TextField yourName = new TextField("yourName",new PropertyModel<String>(acc,"name"));
-        final TextField yourSurname = new TextField("yourSurname",new PropertyModel<String>(acc,"surname"));
-        final TextField yourAge = new TextField("yourAge",new PropertyModel<String>(acc,"age"));
-        final TextField yourPerCode = new TextField("yourPerCode",new PropertyModel<String>(acc,"persCode"));
-        
+        final TextField yourName = new TextField("yourName", new PropertyModel<String>(this, "name"));
+        final TextField yourSurname = new TextField("yourSurname", new PropertyModel<String>(this, "surname"));
+        final TextField yourAge = new TextField("yourAge", new PropertyModel<String>(this, "age"));
+        final TextField yourPerCode = new TextField("yourPerCode", new PropertyModel<String>(this, "persCode"));
+
         Button saveButton = new Button("saveChanges") {
             @Override
             public void onSubmit() {
-            
+
             }
         };
-        
+
         Form profileForm = new Form("profileForm");
         profileForm.add(yourName, yourSurname, yourAge, yourPerCode, saveButton);
-        
-        final PasswordTextField oldPassword = new PasswordTextField("oldPassword",new PropertyModel<String>(acc,"password"));
-        final PasswordTextField newPassword = new PasswordTextField("newPassword",new PropertyModel<String>(acc,"password"));
-        final PasswordTextField repeatNewPassword = new PasswordTextField("repeatNewPassword",new PropertyModel<String>(acc,"password"));
-        
+
+        final PasswordTextField oldPassword = new PasswordTextField("oldPassword", new PropertyModel<String>(acc, "password"));
+        final PasswordTextField newPassword = new PasswordTextField("newPassword", new PropertyModel<String>(acc, "password"));
+        final PasswordTextField repeatNewPassword = new PasswordTextField("repeatNewPassword", new PropertyModel<String>(acc, "password"));
+
         Button changeButton = new Button("savePw") {
             @Override
             public void onSubmit() {
-            
+
             }
         };
-        
+
         Form passwordForm = new Form("passwordForm");
         passwordForm.add(oldPassword, newPassword, repeatNewPassword, changeButton);
-        
+
         add(profileForm, passwordForm);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getPersCode() {
+        return persCode;
+    }
+
+    public void setPersCode(String persCode) {
+        this.persCode = persCode;
+    }
+
+    private SignInSession getMySession() {
+        return (SignInSession) getSession();
+    }
+
+    private Account getAccount(List<Account> accounts, String user) {
+        Account account = new Account();
+        for (Account ac : accounts) {
+            if (ac.getLogin().equals(user)) {
+                account = ac;
+            }
+        }
+        return account;
     }
 }
--- test/dbtest/DBTest.java
+++ test/dbtest/DBTest.java
@@ -2,12 +2,20 @@ package dbtest;
 
 import java.util.List;
 import junit.framework.TestCase;
+import mail.account.Account;
 import tour.order.OrderDao;
 import tour.order.models.Country;
 import tour.order.models.OrderObject;
+import mail.account.AccountDAO;
 
 public class DBTest extends TestCase {
 
+    public void testAccounts(){
+        AccountDAO accountDao = new AccountDAO();
+        List<Account> accounts = accountDao.getAccountList();
+        assertFalse(accounts.isEmpty());
+    }
+    
     public void testCountrysDB(){
         OrderDao dao =new OrderDao();
         List<OrderObject> countrys = dao.getCountries();

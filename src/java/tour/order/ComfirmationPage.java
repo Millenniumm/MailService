11ebd1f40739120545396c3d370e5082c89d0fa7
ktelfon <ktelfon@gmail.com>
2014-01-05 21:46:07 +0200

    Update ComfirmationPage.java

--- src/java/tour/order/ComfirmationPage.java
+++ src/java/tour/order/ComfirmationPage.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tour.order;
 
 import tour.main.MainPage;
@@ -15,10 +11,7 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.behavior.* ;
 import tour.header.HeaderPanel;
 
-/**
- *
- * @author Deniss
- */
+
 public final class ComfirmationPage extends WebPage {
 
     private OrderDao orderDao;

bec759e19d027223196c3a46b5e84d28af84a451
ktelfon <ktelfon@gmail.com>
2014-01-05 20:47:16 +0200

    Update UserOrderHistory.java

--- src/java/tour/profile/history/UserOrderHistory.java
+++ src/java/tour/profile/history/UserOrderHistory.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package tour.profile.history;
 
@@ -21,10 +16,7 @@ import tour.header.HeaderPanel;
 import tour.order.OrderDao;
 import tour.order.models.Order;
 
-/**
- *
- * @author Deniss
- */
+
 public final class UserOrderHistory extends WebPage {
 
     public UserOrderHistory() {

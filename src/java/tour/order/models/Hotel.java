f37e69d011ecd160358dcf2eb9f425b9e9d6eb5e
ktelfon <ktelfon@gmail.com>
2014-01-05 20:46:19 +0200

    Update Hotel.java

--- src/java/tour/order/models/Hotel.java
+++ src/java/tour/order/models/Hotel.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package tour.order.models;
 
-/**
- *
- * @author Deniss
- */
 public class Hotel extends OrderObject{
 
     private String name;

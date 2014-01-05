2911344d8448633c22399486e353f6963410180e
ktelfon <ktelfon@gmail.com>
2014-01-05 20:42:43 +0200

    Update OrderDao.java

--- src/java/tour/order/OrderDao.java
+++ src/java/tour/order/OrderDao.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tour.order;
 
 import tour.order.models.Hotel;
@@ -15,10 +11,7 @@ import java.util.List;
 import tour.dbconnection.DBConnection;
 import tour.order.models.Order;
 
-/**
- *
- * @author Deniss
- */
+
 public class OrderDao {
 
     public static void removeOrderById(String id) {

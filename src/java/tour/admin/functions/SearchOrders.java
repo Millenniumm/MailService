2ef90651c9d3167529ca49b42c87f14e4c8c3f9b
ktelfon <ktelfon@gmail.com>
2014-01-05 21:47:43 +0200

    Update SearchOrders.java

--- src/java/tour/admin/functions/SearchOrders.java
+++ src/java/tour/admin/functions/SearchOrders.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tour.admin.functions;
 
 import tour.menupanel.MenuPanel;
@@ -15,10 +11,7 @@ import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.model.Model;
 import tour.header.HeaderPanel;
 
-/**
- *
- * @author Deniss
- */
+
 public final class SearchOrders extends WebPage {
 
     private TextField<String> username;
